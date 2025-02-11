55953131313fcb0c91ede4adece922cfa6f826d5
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-12 01:29:50 +0530

    Add Preloader Page

--- app/src/main/java/lk/sankaudeshika/androidfixerbee/MainActivity.java
+++ app/src/main/java/lk/sankaudeshika/androidfixerbee/MainActivity.java
@@ -1,6 +1,10 @@
 package lk.sankaudeshika.androidfixerbee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+//        Loading PreLoader Page
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+
+
+            int x=0;
+            @Override
+            public void run() {
+                for( int i = 0;i<100; i++){
+                    x = i;
+                    try {
+                      Thread.sleep(50);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            progressBar.setProgress(x,true);
+                        }
+                    });
+                }
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        }).start();
+
+
+
+
     }
 }
