31aa689df01ed6805cb4c6d02f85378de9ef9874
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-16 13:05:58 +0530

    Designed Regsiter Page

--- app/src/main/java/lk/sankaudeshika/androidfixerbee/RegisterActivity.java
+++ app/src/main/java/lk/sankaudeshika/androidfixerbee/RegisterActivity.java
@@ -1,10 +1,9 @@
 package lk.sankaudeshika.androidfixerbee;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,33 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        View view = getLayoutInflater().inflate(R.layout.login_sub_component,null);
+        View RegisterView = getLayoutInflater().inflate(R.layout.register_sub_component,null);
 
-      LinearLayout login_lLinearLayout =  findViewById(R.id.registerLienarLayout);
-      login_lLinearLayout.addView(view);
-
-//      Link Regsiter Activity
-        TextView goRegisterText = findViewById(R.id.textView8);
-        goRegisterText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivity(i);
-            }
-        });
+        LinearLayout linearLayout = findViewById(R.id.registerLienarLayout);
+        linearLayout.addView(RegisterView);
 
     }
 }
