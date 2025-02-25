a6f0ba54090cb928ed7751c28b1d178ffb405f43
SankaUdeshika <sankaudeshika123@gmail.com>
2025-02-25 14:12:04 +0530

    add Cancel Process to the My Bookings

--- app/src/main/java/lk/sankaudeshika/androidfixerbee/ui/mybookings/MyBookingFragment.java
+++ app/src/main/java/lk/sankaudeshika/androidfixerbee/ui/mybookings/MyBookingFragment.java
@@ -6,8 +6,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,11 +19,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -36,6 +42,7 @@ import lk.sankaudeshika.androidfixerbee.ServiceViewActivity;
 import lk.sankaudeshika.androidfixerbee.model.Books;
 import lk.sankaudeshika.androidfixerbee.model.ServerURL;
 import lk.sankaudeshika.androidfixerbee.model.Service;
+import lk.sankaudeshika.androidfixerbee.ui.home.HomeFragment;
 
 public class MyBookingFragment extends Fragment {
 
@@ -62,6 +69,7 @@ public class MyBookingFragment extends Fragment {
                         IterateBooksArrayList = new ArrayList<>();
                         for (DocumentSnapshot documentItem: documentList) {
                             Books books = new Books(
+                                    documentItem.getId(),
                                     documentItem.getString("description"),
                                     documentItem.getString("status"),
                                     documentItem.getString("time"),
@@ -97,6 +105,8 @@ class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.BookingViewHold
         TextView bookdate;
         TextView booktime;
         TextView bookstatus;
+        TextView bookdescription;
+        Button cancelBtn ;
         public BookingViewHolder(@NonNull View itemView){
             super(itemView);
             CompanyName =  itemView.findViewById(R.id.vendorCompany);
@@ -104,15 +114,17 @@ class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.BookingViewHold
             bookdate = itemView.findViewById(R.id.bookdate);
             booktime = itemView.findViewById(R.id.booktime);
             bookstatus = itemView.findViewById(R.id.bookstatus);
+            bookdescription = itemView.findViewById(R.id.BookDescription);
+            cancelBtn = itemView.findViewById(R.id.cancelBtn);
         }
     }
 
     ArrayList<Books> BookingArraylist;
     Context context;
+    Fragment fragment;
 
     public BookingAdapter(ArrayList bookingArraylist1, Context context) {
         BookingArraylist = bookingArraylist1;
-        this.context = context;
     }
 
 
@@ -128,11 +140,36 @@ class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.BookingViewHold
     @Override
     public void onBindViewHolder(@NonNull BookingViewHolder holder, int position) {
         Books books = BookingArraylist.get(position);
-        holder.CompanyName.setText(books.getDescription());
+        holder.bookdescription.setText(books.getDescription());
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("vendor").document(books.getVendor_id())
+                        .get()
+                                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    holder.CompanyName.setText(task.getResult().getString("sub_category"));
+                                    }
+                                });
         holder.vendorMobile.setText(books.getVendorMobile());
         holder.bookdate.setText(books.getDate());
         holder.booktime.setText(books.getTime());
         holder.bookstatus.setText(books.getStatus());
+        holder.cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                firestore.collection("booking")
+                        .document(books.getBookId())
+                        .delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                // Refresh the current fragment
+                               new AlertDialog.Builder(view.getContext()).setTitle("Delete Success").show();
+
+                            }
+                        });
+            }
+        });
     }
 
     @Override
